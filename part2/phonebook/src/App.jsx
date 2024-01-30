400828a5cd9541b94ee5139df15d972760858d13
Roberto Milan <18741545+MSRobert96@users.noreply.github.com>
2024-01-30 16:20:32 +0000

    part2 2.12-2.15 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,25 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import personsService from './services/personsService'
 
-const Filter = ({value, onChange}) => 
+const Filter = ({ value, onChange }) =>
     <div>
-        filter shown with 
-        <input type="text" value={value} onChange={onChange} />
+        filter shown with <input type="text" value={value} onChange={onChange} />
     </div>
 
-const PersonForm = ({newName, newNumber, onSubmit, onNameChange, onNumberChange}) => 
+const PersonForm = ({ newName, newNumber, onSubmit, onNameChange, onNumberChange }) =>
     <form onSubmit={onSubmit}>
         <div>name: <input value={newName} onChange={onNameChange} /></div>
         <div>number: <input value={newNumber} onChange={onNumberChange} /></div>
         <div><button type="submit">add</button></div>
     </form>
 
-const Persons = ({ persons }) => 
-    <>
-        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
-    </>
-    
+const Persons = ({ persons, handleDelete }) =>
+    persons.map(person =>
+        <div key={person.id}>
+            <span>{person.name} {person.number}</span>
+            <button onClick={handleDelete(person)}>delete</button>
+        </div>
+    )
 
 const App = () => {
     const [persons, setPersons] = useState([])
@@ -27,27 +28,51 @@ const App = () => {
     const [search, setSearch] = useState('')
 
     useEffect(() => {
-        axios
-            .get(' http://localhost:3001/persons')
-            .then(response => setPersons(response.data))
+        personsService.getAll()
+            .then(initialPersons => setPersons(initialPersons))
     }, [])
 
     const addPerson = (event) => {
         event.preventDefault()
-        console.log(persons)
-        if (persons.filter(person => person.name == newName).length > 0) {
-            alert(`${newName} is already added to phonebook`)
-            return
+        let existingPerson = persons.find(p => p.name == newName)
+
+        if (existingPerson !== undefined) {
+
+            if (!window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`))
+                return // abort update
+
+            // update existing person
+            personsService.update(existingPerson.id, { ...existingPerson, number: newNumber })
+                .then(updatedPerson => {
+                    setPersons(persons.map(p => p.id === updatedPerson.id ? updatedPerson : p))
+                    setNewName('')
+                    setNewNumber('')
+                })
         }
 
-        setPersons(persons.concat({ name: newName, number: newNumber }))
-        setNewName('')
-        setNewNumber('')
+        else {
+            // create new person
+            personsService.create({ name: newName, number: newNumber })
+                .then(createdPerson => {
+                    setPersons(persons.concat(createdPerson))
+                    setNewName('')
+                    setNewNumber('')
+                })
+        }
+
     }
 
-    const personsToShow = search == ''
+    const handlePersonDelete = (person) => () => {
+        if (!window.confirm(`Delete ${person.name} ?`))
+            return
+
+        personsService.remove(person.id)
+            .then(deletedPerson => setPersons(persons.filter(person => person.id != deletedPerson.id)))
+    }
+
+    const personsToShow = search.trim() == ''
         ? persons
-        : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()) )
+        : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
 
     return (
         <div>
@@ -55,7 +80,7 @@ const App = () => {
             <Filter value={search} onChange={(event => setSearch(event.target.value))} />
 
             <h3>add a new</h3>
-            
+
             <PersonForm
                 onSubmit={addPerson}
                 newName={newName}
@@ -65,7 +90,7 @@ const App = () => {
 
             <h3>Numbers</h3>
 
-            <Persons persons={personsToShow} />
+            <Persons persons={personsToShow} handleDelete={handlePersonDelete} />
         </div>
     )
 }
