b933ee2ca5330a321ae4c86eba1c7441f3702ed3
Roberto Milan <18741545+MSRobert96@users.noreply.github.com>
2024-02-09 15:28:02 +0000

    part4 4.4-4.7 bloglist tests advanced

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -8,7 +10,28 @@ const totalLikes = (blogs) => {
         : 0
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs && blogs.length > 0
+        ? blogs.toSorted((a, b) => b.likes - a.likes)[0]
+        : null
+}
+
+const mostBlogs = (blogs) => {
+    let grouped = _.groupBy(blogs, blog => blog.author)
+    let maxAuthor = _.maxBy(_.toPairs(grouped), author => author[1].length)
+    return { author: maxAuthor[0], blogs: maxAuthor[1].length }
+}
+
+const mostLikes = (blogs) => {
+    let grouped = _.groupBy(blogs, blog => blog.author)
+    let maxAuthor = _.maxBy(_.toPairs(grouped), author => author[1].reduce((tot, b) => tot + b.likes, 0))
+    return { author: maxAuthor[0], likes: maxAuthor[1].reduce((tot, b) => tot + b.likes, 0) }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
